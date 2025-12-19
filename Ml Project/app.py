447729993e5197943ff21f9f40f6db4bc095e009
ahmedshingare <ahmedshingare@gmail.com>
2025-12-20 02:21:45 +0530

    Update app.py

--- Ml Project/app.py
+++ Ml Project/app.py
@@ -16,56 +16,28 @@ def fetch_poster(movie_id):
 
 # ---------- Recommendation logic ----------
 def recommend(movie, n):
-    index = movies[movies['title'] == movie].index[0]
+    index = movies[movies["title"] == movie].index[0]
     distances = sorted(
         list(enumerate(similarity[index])),
         reverse=True,
         key=lambda x: x[1]
     )
 
-    recommended_movie_names = []
-    recommended_movie_posters = []
-
+    names, posters = [], []
     for i in distances[1:n+1]:
-        movie_id = movies.iloc[i[0]]["movie_id"]  # safer
-        recommended_movie_posters.append(fetch_poster(movie_id))
-        recommended_movie_names.append(movies.iloc[i[0]]["title"])
+        movie_id = movies.iloc[i[0]]["movie_id"]
+        names.append(movies.iloc[i[0]]["title"])
+        posters.append(fetch_poster(movie_id))
 
-    return recommended_movie_names, recommended_movie_posters
+    return names, posters
 
 
-# ---------- UI ----------
-st.header("🎬 Movie Recommendation System")
+# ---------- Page Config ----------
+st.set_page_config(page_title="Movie Recommender", layout="wide")
+
+# ---------- Sidebar ----------
+st.sidebar.title("🎥 Movie Recommender")
+st.sidebar.write("Get movie recommendations using ML similarity")
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-movies = pickle.load(open(os.path.join(BASE_DIR, "movie_list.pkl"), "rb"))
-similarity = pickle.load(open(os.path.join(BASE_DIR, "similarity.pkl"), "rb"))
-
-movie_list = movies["title"].values
-
-selected_movie = st.selectbox(
-    "Type or select a movie from the dropdown",
-    movie_list
-)
-
-# ---------- Slider ----------
-num_recommendations = st.slider(
-    "Number of recommendations",
-    min_value=3,
-    max_value=10,
-    value=5
-)
-
-# ---------- Button with Spinner ----------
-if st.button("Show Recommendation"):
-    with st.spinner("Finding similar movies for you... 🎬"):
-        recommended_movie_names, recommended_movie_posters = recommend(
-            selected_movie, num_recommendations
-        )
-
-    cols = st.columns(num_recommendations)
-    for col, name, poster in zip(cols, recommended_movie_names, recommended_movie_posters):
-        with col:
-            st.image(poster)
-            st.caption(name)
+movies = pickle.load(open(os.
